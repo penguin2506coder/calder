7055fabddf0e73f5bd5dbdd2e937849eea5b13a1
penguin2506coder <penguin2506coder@penguincoding.org>
2024-08-05 14:57:15 -0400

    Add files via upload

---
+++ calder func riveiw.py
@@ -0,0 +1,5 @@
+def greeting(name):
+    print("hello",name)
+
+name1=input("what is your name: ")
+greeting(name1)
---
+++ calder review day.py
@@ -0,0 +1,22 @@
+'''a=8
+b="hello"
+print(str(a)+b)
+c=4
+d="8"
+print(int(d)+c)'''
+
+
+import random
+num=random.randint(0,100)
+
+if num>50:
+    type="stupid"
+
+if num<50:
+    type="not skibbity"
+print("you are",type)
+
+'''if num>90:
+    print("you won",num, "dollars")
+else:
+   print ("you suck at wheel of fortune")'''
